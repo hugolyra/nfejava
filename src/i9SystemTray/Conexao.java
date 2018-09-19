568114bab3e5ab53b9f54b460c2e3b0b6f30ba85
Hugo Lyra <huugocosta@gmail.com>
2018-09-19 14:17:33 -0300

    Update Conexao.java

--- src/i9SystemTray/Conexao.java
+++ src/i9SystemTray/Conexao.java
@@ -9,16 +9,16 @@ import javax.swing.JOptionPane;
 
 public class Conexao {
 
-    private static final String USUARIO = "maicon";
-    private static final String SENHA = "1q2w3e!Q@W#E";
-    private static final String URL = "jdbc:mysql://safe.acenecontabilidade.com.br:3306/i9";
+    private static final String USUARIO = 
+    private static final String SENHA = 
+    private static final String URL = 
     private static final String DRIVER = "com.mysql.jdbc.Driver";
 
     // Conectar ao banco
     public static Connection abrir() throws Exception {
         // Registrar o driver
         Class.forName(DRIVER);
-        // Capturar a conex„o
+        // Capturar a conex√£o
         Connection conn = DriverManager.getConnection(URL, USUARIO, SENHA);
         // Retorna a conexao aberta
         return conn;
